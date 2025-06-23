c3274ab59b49b556bb701374037969a9a5f58b19
rhit-mccarrm <mccarrm@rose-hulman.edu>
2025-06-23 10:38:17 -0500

    WIP

--- Lethally.Creative.py
+++ Lethally.Creative.py
@@ -6,7 +6,7 @@ import my_character
 import random
 import time
 
-class Character:
+class Knight:
     def __init__(self, screen: pygame.Surface, x, y):
         self.screen = screen
         self.x = x
@@ -16,6 +16,10 @@ class Character:
     def draw(self):
         self.screen.blit(self.image_still, (self.x, self.y))
 
+class Platform:
+    def __init__(self, x, y, width, height, color=(0,0,0)):
+        
+
 WIDTH = 1280
 HEIGHT = 720
 WHITE = (255,255,255)
@@ -24,45 +28,21 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 YELLOW = (255,255,0)
-GROUND_HEIGHT= 100
 
-class Character:
-    def __init__(self, screen, x, y):
-        self.screen = screen
-        self.x = x
-        self.y = y
-        self.y = y
-        self.width = 50
-        self.height = 50
-        self.speed = 5
-        self.rect = pygame.Rect(x, y, self.width, self.height)
-
-    def update(self ):
-        keys = pygame.key.get_pressed()
-        old_x, old_y = self.x, self.y
-
-        if keys[pygame.K_LEFT] and self.x > 0:
-            self.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.x < WIDTH - self.width:
-            self.x += self.speed
-
-        
 
 
 def main():
-    # turn on pygame
     pygame.init()
-
     image1 = pygame.image.load("output-onlinepngtools.jpg")
     color1 = pygame.Color('black')
-    ground_height = 100
-    ground_rect = pygame.Rect(0, HEIGHT - ground_height)
-    # create a screen
+    ground_height = 10
+    ground_rect = pygame.Rect(0, 550, ground_height, ground_height)
     pygame.display.set_caption("lethally Project")
     # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((1280, 720))
     # creates a Character from the my_character.py file
-    character = my_character.Character(screen, 100, 500)
+    knight = my_character.Character(screen, 100, 500)
+
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -75,17 +55,14 @@ def main():
 
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_LEFT]:
-            character.x = character.x - 5
+            knight.x = knight.x - 5
         if pressed_keys[pygame.K_RIGHT]:
-            character.x = character.x + 5
+            knight.x = knight.x + 5
         if pressed_keys[pygame.K_SPACE]:
-            character.y = character.y - 15
+            knight.y = knight.y - 15
 
-        character.draw()
-
-
-
-    pygame.draw.rect( screen, color1 , gorund_rect )
+        knight.draw()
+        pygame.draw.rect(screen, color1 , ground_rect)
             # TODO: Add you events code
 
         # TODO: Fill the screen with whatever background color you like!
