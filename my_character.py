7aadb136c6facd52ba3c8100f5bf7a0ba39e64b2
rhit-mccarrm <mccarrm@rose-hulman.edu>
2025-06-23 10:54:04 -0500

    New my_character

--- my_character.py
+++ my_character.py
@@ -7,12 +7,10 @@ class Character:
         self.screen = screen
         self.x = x
         self.y = y
+        self.image_still = pygame.image.load("Idle.png")
 
     def draw(self):
-        pygame.draw.rect(self.screen, "blue", (self.x, self.y, 20, 20))
-        pygame.draw.circle(self.screen, "red", (self.x + 5, self.y + 5), 3)
-        pygame.draw.circle(self.screen, "red", (self.x + 15, self.y + 5), 3)
-
+        self.screen.blit(self.image_still, (self.x, self.y))
 
 # This function is called when you run this file, and is used to test the Character class individually.
 # When you create more files with different classes, copy the code below, then
@@ -25,6 +23,21 @@ def test_character():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        for event in pygame.event.get():
+            # TODO 4:   Make the pygame.QUIT event stop the game.
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_LEFT]:
+            character.x = character.x - 2
+        if pressed_keys[pygame.K_RIGHT]:
+            character.x = character.x + 2
+        if pressed_keys[pygame.K_UP]:
+            character.y = character.y - 2
+        if pressed_keys[pygame.K_DOWN]:
+            character.y = character.y + 2
 
         screen.fill("white")
         character.draw()
