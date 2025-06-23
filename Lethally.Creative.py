7f29a16519ab1c114f4a681917093c98518d5d3e
rhit-mccarrm <mccarrm@rose-hulman.edu>
2025-06-23 10:05:35 -0500

    Four Knights can move and jump

--- Lethally.Creative.py
+++ Lethally.Creative.py
@@ -4,6 +4,15 @@ import my_character
 import random
 import time
 
+class Character:
+    def __init__(self, screen: pygame.Surface, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image_still = pygame.image.load("Idle.png")
+
+    def draw(self):
+        self.screen.blit(self.image_still, (self.x, self.y))
 
 def main():
     # turn on pygame
@@ -16,7 +25,7 @@ def main():
     # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((1280, 720))
     # creates a Character from the my_character.py file
-    character = my_character.Character(screen, 100, 100)
+    character = my_character.Character(screen, 100, 500)
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -25,8 +34,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.blit(image1, (0, 0))
 
-        screen.blit(image1, (0,0))
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_LEFT]:
+            character.x = character.x - 5
+        if pressed_keys[pygame.K_RIGHT]:
+            character.x = character.x + 5
+        if pressed_keys[pygame.K_SPACE]:
+            character.y = character.y - 15
+
+        character.draw()
+
+
 
             # TODO: Add you events code
 
